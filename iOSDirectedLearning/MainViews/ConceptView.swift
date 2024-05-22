b24b1778583343e930f5e32b1c779833cd61ec53
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-05-22 17:16:05 +0200

    Add ConceptView

---
+++ iOSDirectedLearning/MainViews/ConceptView.swift
@@ -0,0 +1,50 @@
+//
+//  ConceptView.swift
+//  iOSDirectedLearning
+//
+//  Created by Szymon Michalak on 22/05/2024.
+//
+
+import SwiftUI
+
+struct ConceptView: View {
+    let content: AnyView
+    
+    var body: some View {
+        content
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        // TODO: - Add this item to a list of favorites.
+                    } label: {
+                        Label("Add to Favorites", systemImage: "heart")
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Section("What would you like to do?") {
+                            Button {
+                                // TODO: - View Code on Github
+                            } label: {
+                                Label("See code on Github", systemImage: "swift")
+                            }
+                            Button {
+                                // TODO: - Add this item to a list of favorites.
+                            } label: {
+                                Label("Add to Favorites", systemImage: "heart")
+                            }
+                        }
+                    } label: {
+                        Label("Menu", systemImage: "ellipsis.circle")
+                    }
+                }
+            }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ConceptView(content: AnyView(Text("lol")))
+    }
+}
