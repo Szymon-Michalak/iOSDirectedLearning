e9a4ddaff61aab8d50514742c9cd714909952070
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-05-22 16:07:56 +0200

    Add Random Concepts view and logic

--- iOSDirectedLearning/MainViews/CategoriesView.swift
+++ iOSDirectedLearning/MainViews/CategoriesView.swift
@@ -5,17 +5,122 @@ import SwiftUI
 struct CategoriesView: View {
     @EnvironmentObject var modelData: ModelData
 
+    @State private var colors: [Color] = [.red, .blue, .green, .yellow]
+    
+    
+    
     var body: some View {
         NavigationStack {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(modelData.getRandomExamples, id: \.id) { example in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .fill(example.innerExample.complexity.color)
+                                .padding(.horizontal, 5)
+                                .containerRelativeFrame(.horizontal)
+                            VStack(alignment: .leading) {
+                                Text(example.innerExample.title)
+                                    .font(.title2.bold())
+                                    .lineLimit(3)
+                                    .foregroundStyle(.white)
+                                Text(example.innerExample.description)
+                                    .lineLimit(0)
+                                    .foregroundStyle(.white)
+                            }
+                            .containerRelativeFrame(.horizontal)
+                            .padding(.horizontal, 5)
+                        }
+                    }
+                }
+                .scrollTargetLayout()
+                .overlay(alignment: .bottom) {
+                    PagingIndicator(
+                        activeTint: .white,
+                        inActiveTint: .black.opacity(0.25)
+                    )
+                }
+            }
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
+            .frame(height: 220)
+            .safeAreaPadding(.vertical, 15)
+            .safeAreaPadding(.horizontal, 25)
+            
             List(modelData.categories) { category in
                 NavigationLink(destination: CategoryView(category: category)) {
                     Text(category.title)
                 }
             }
+            .clipShape(.rect(cornerRadius: 15))
+            .padding(15)
+            .navigationTitle("Explore Random Concepts")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 #Preview {
     CategoriesView()
+        .environmentObject(ModelData())
+}
+
+
+
+//
+//  Home.swift
+//  AnimatedPageIndicator
+//
+//  Created by Balaji Venkatesh on 01/01/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    /// View Properties
+    @State private var colors: [Color] = [.red, .blue, .green, .yellow]
+    @State private var opacityEffect: Bool = false
+    @State private var clipEdges: Bool = false
+    var body: some View {
+        VStack {
+            /// Paging View
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(colors, id: \.self) { color in
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(color.gradient)
+                            .padding(.horizontal, 5)
+                            .containerRelativeFrame(.horizontal)
+                    }
+                }
+                .scrollTargetLayout()
+                .overlay(alignment: .bottom) {
+                    PagingIndicator(
+                        activeTint: .white,
+                        inActiveTint: .black.opacity(0.25),
+                        opacityEffect: opacityEffect,
+                        clipEdges: clipEdges
+                    )
+                }
+            }
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
+            .frame(height: 220)
+            .safeAreaPadding(.vertical, 15)
+            .safeAreaPadding(.horizontal, 25)
+            
+            List {
+                Section("Options") {
+                    Toggle("Opacity Effect", isOn: $opacityEffect)
+                    Toggle("Clip Edges", isOn: $clipEdges)
+                    
+                    Button("Add Item") {
+                        colors.append(.purple)
+                    }
+                }
+            }
+            .clipShape(.rect(cornerRadius: 15))
+            .padding(15)
+        }
+    }
 }
--- iOSDirectedLearning/Models/ModelData.swift
+++ iOSDirectedLearning/Models/ModelData.swift
@@ -4,4 +4,6 @@ import SwiftUI
 
 final class ModelData: ObservableObject {
     @Published var categories: [Category] = CategoryInitializer.categories
+    
+    @Published var getRandomExamples = CategoryInitializer.categories.map { $0.concepts }.flatMap { $0 }.map { $0.examples }.flatMap { $0 }.map { AnyExample($0) }.shuffled().prefix(upTo: 5).map { $0 }
 }
