06ff4e8a30f3d259cff83f65018bd74b6b398a0c
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-05-28 13:35:38 +0200

    Add Gradient background to main view

--- iOSDirectedLearning/MainViews/HomeView.swift
+++ iOSDirectedLearning/MainViews/HomeView.swift
@@ -3,39 +3,39 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var modelData: ModelData
 
-    @State private var colors: [Color] = [.red, .blue, .green, .yellow]
-
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack {
-                    HeroPagingView(modelData: modelData)
-                        .scrollTargetBehavior(.viewAligned)
-                        .scrollIndicators(.hidden)
-                        .frame(height: 220)
-                        .safeAreaPadding(.vertical, 15)
-                        .safeAreaPadding(.horizontal, 25)
-
-                    ForEach(modelData.categories) { category in
-                        NavigationLink(destination: CategoryView(category: category)) {
-                            RowView(content: category)
+            ZStack {
+                Rectangle()
+                    .fill(LinearGradient(gradient:
+                                            Gradient(colors: [
+                                                .purple.opacity(0.15),
+                                                .purple.opacity(0.55)
+                                            ]), startPoint: .top, endPoint: .bottom))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .ignoresSafeArea()
+                VStack {
+                    LazyVStack {
+                        HeroPagingView(modelData: modelData)
+                            .scrollTargetBehavior(.viewAligned)
+                            .scrollIndicators(.hidden)
+                            .frame(height: 220)
+                            .safeAreaPadding(.vertical, 15)
+                            .safeAreaPadding(.horizontal, 25)
+                            .shadow(radius: 10)
+                        ScrollView {
+                            ForEach(modelData.categories) { category in
+                                NavigationLink(destination: CategoryView(category: category)) {
+                                    RowView(content: category)
+                                }
+                            }
+                            .padding(.horizontal, 20)
                         }
                     }
-                    .padding(.horizontal, 20)
-
-                    List(modelData.categories) { category in
-                        NavigationLink(destination: CategoryView(category: category)) {
-                            Text(category.title)
-                        }
-                        Text("Coming Soon...")
-                    }
-                    .listStyle(.plain)
-                    .clipShape(.rect(cornerRadius: 15))
-                    .padding(15)
-                    .navigationTitle("Explore iOS Concepts")
-                    .navigationBarTitleDisplayMode(.inline)
                 }
             }
+            .navigationTitle("iOS Directed Learning")
+            .toolbarTitleDisplayMode(.inline)
         }
     }
 }
