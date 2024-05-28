b1f005471e690dd4fca45ec013ac924e807c7250
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-05-28 13:35:49 +0200

    Add Empty state to category

--- iOSDirectedLearning/MainViews/CategoryView.swift
+++ iOSDirectedLearning/MainViews/CategoryView.swift
@@ -4,9 +4,41 @@ struct CategoryView: View {
     let category: Category
 
     var body: some View {
-        List(category.topics) { topic in
-            NavigationLink(destination: TopicView(topic: topic)) {
-                Text(topic.title)
+            if category.topics.isEmpty {
+                ContentUnavailableView {
+                    Label {
+                        Text("No Concepts Yet")
+                            .font(.body)
+                            .foregroundColor(.primary)
+                    } icon: {
+                        Image(category.image ?? "category/swift")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50)
+                            .clipShape(Circle())
+                    }
+                } description: {
+                    Text("Stay tuned for updates. We will be adding more concepts soon!")
+                }
+            } else {
+                ScrollView {
+                VStack {
+                    ForEach(category.topics) { topic in
+                        DisclosureGroup(topic.title) {
+                            ForEach(topic.concepts, id: \.id) { concept in
+                                NavigationLink(destination: ConceptView(concept: concept)) {
+                                    ConceptRowView(concept: concept)
+                                }
+                            }
+                        }
+//                        NavigationLink(destination: TopicView(topic: topic)) {
+//                            RowView(content: topic)
+//                        }
+                    }
+
+                    Spacer()
+                }
+                .padding()
             }
         }
     }
